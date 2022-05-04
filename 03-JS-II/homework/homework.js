1aae60312a1fa42ea0a2e062f82bd24e18ed5ea5
cristianbernal1 <cristianbernal1@outlook.com>
2022-05-03 22:23:05 -0500

    new changes

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -4,13 +4,20 @@ function obtenerMayor(x, y) {
   // "x" e "y" son números enteros (int).
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
-  // Tu código:
-}
+  // Tu código: 
+  if( x > y ) {
+    return x;  
+   }
+  return y;
+} 
 
 function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
+  if (edad>= 18) {return "Allowed";
+  }
+  return "Not allowed"
 }
   
 function conection(status) {
@@ -19,7 +26,11 @@ function conection(status) {
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
-}
+  if (status === 1) {return "Online";
+  } else if (status === 2) { return "Away";
+  }   else {return "Offline"} 
+  }  
+
 
 function saludo(idioma) {
   // Devuelve un saludo en tres diferentes lenguajes:
@@ -28,7 +39,15 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+  if (idioma === "aleman") {return "Guten Tag!";
+} else if (idioma === "mandarin") {return "Ni Hao!";
 }
+  else if (idioma === "ingles") {return "Hello!"
+} 
+  else {return "Hola!"
+}
+}
+
 
 function colors(color) {
   //La función recibe un color. Devolver el string correspondiente:
@@ -44,12 +63,15 @@ function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  return numero === 10 || numero === 5
+
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  return numero > 50 && numero > 20 
 }
 
 function esEntero(numero) {
@@ -60,6 +82,7 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+  {return numero %1 === 0}
 }
 
 function fizzBuzz(numero) {
